3209c4e72cf099dc5ca7b2fc23ea4901d5354b26
francescapetra <francescalaurapetra@gmail.com>
2021-04-07 14:46:59 +0200

    completo

--- script/script.js
+++ script/script.js
@@ -2,10 +2,19 @@
   {
     el:"#root",
     data:{
-      message: "",
-      miaClasse : "red",
+      mail: [],
     },
     methods:{
     },
+    mounted: function(){
+      for (var i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then( (response) => {
+        const result = response.data;
+        this.mail.push(result);
+        console.log(result);
+        });
+      }
+    }
   }
 );
